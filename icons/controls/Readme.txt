a9f6712428d4f76ffa7c066763a5f6eaf3010477
PxlCtzn <legrand.baptiste@gmail.com>
2019-03-29 17:03:03 +0100

    Adding controller Icon

---
+++ icons/controls/Readme.txt
@@ -0,0 +1,114 @@
+Hey there!
+
+Hope you make good use of this pack. You can use all these assets in any project you want to (be it commercial or not).
+All of the assets are in the public domain under Creative Commons 0 (CC0)
+
+In this pack you will find over 500 buttons including:
+
+Xbox 360 controller
+Xbox One controller
+Play Station 3 controller
+Play Station 4 controller
+Play Station Move
+PS Vita
+Vive Controller
+Oculus Controllers & Remote
+Wii Controller
+Wii U Controller
+Nintentdo Switch
+Steam Controller (Updated to commercial version)
+Ouya
+Keyboard and mouse buttons (Both in black and white including blanks)
+Directional arrows for thumb sticks and movement keys
+Touch Screen Gestures
+
+----------------------------------
+
+I am "Nicolae Berbece" (also known as Xelu), I founded "Those Awesome Guys" and made "Move or Die" which is out right now on Steam.
+You can contact me at nick@thoseawesomeguys.com
+
+Feel free to credit me in case you use anything in this pack, but don't worry, I won't mind if you don't. ;)
+
+Please share this pack with other fellow developers in need of such assets! In the spirit of good old chain mail, if you share this pack with 5 fellow devs, your game's steam review score will rise by 7% and a notable twitch streamer will pick it up for his stream.
+
+Keep making awesome things!!!
+
+~Nick
+
+
+
+
+
+Here is a semi-updated list of games using these prompts:
+----------------------------
+Mega Man Legacy
+Shadow Warrior 2
+Wonder Boy the dragon's trap
+Postal 2
+Postal Redux
+RWBY
+Aztez
+Disney Afternoon
+Heat Signature
+Turbo Dismount
+Black Future 88
+Fallen Legion
+PikuNiku
+Fru
+Blockships
+20XX
+Mike Dies
+Snake Pass
+Obduction
+Avaria VS
+Blast Zone! Tournament
+100ft Robot Golf
+B.O.O.M . - You Win
+Battle Chef Brigade
+Phantom Brigade
+Wytchwood
+Mulaka
+Airheart - Tales of broken wings
+Redirection
+Defender's Quest
+Akuto "Mad World"
+Whisper
+Vostok Inc.
+Remnants of Naezith
+Mothergunship
+Roundabout
+Cosmos Quickstop
+Tune Tank
+Infinite Adventures
+Arena 3D
+Hoverloop
+Altero
+Super Comboman
+Disc Jam
+Cooking Simulator
+The Hatching
+World to the West
+Mayan Death Robots
+Sentris
+Carto
+Unbox
+Fort Triumph
+Insane Robots
+Super Daryl Deluxe
+Induction
+The Flock
+Binary Trigger
+Fractal Space
+Deputy dangle
+Tumblestone
+The chronicles of Kyra
+Ghost Knight Victis
+Solbrain Knight of Darkness
+SSMP
+Distance
+Idarb
+Earthlock
+Everspace
+Pylon Rogue
+The Church in the darkness
+Sword n' Board
--- js/game/controllers.js
+++ js/game/controllers.js
@@ -1,32 +1,117 @@
+function getKey(control_type, key_id)
+{
+    if(control_type === "keyboard")
+    {
+        return configKeyboard[key_id];
+    }
+}
+updateControlsDisplay = (control_type) => {
+    let controls = [
+        {
+            "label" : "Move left",
+            "id" : "LEFT"
+        },
+        {
+            "label" : "Move right",
+            "id" : "RIGHT"
+        },
+        {
+            "label" : "Rotate clockwise",
+            "id" : "ROTATE_CLOCKWISE"
+        },
+        {
+            "label" : "Rotate counterclockwise",
+            "id" : "ROTATE_COUNTERCLOCKWISE"
+        },
+    
+        {
+            "label" : "Hold",
+            "id" : "HOLD"
+        },
+        {
+            "label" : "Pause",
+            "id" : "PAUSE"
+        },
+        {
+            "label" : "Soft drop",
+            "id" : "SOFT_DROP"
+        },
+        {
+            "label" : "Hard drop",
+            "id" : "HARD_DROP"
+        },
+    ];
+
+    let control_list = document.getElementById("control-list");
+    // We clear controls display
+    control_list.innerHTML = "";
+
+    controls.forEach((control) => {
+        let control_item = document.createElement("li");
+        control_item.class     = "control-list-item";
+        control_item.innerHTML = 
+            "<span class='label'>" + control.label + "</span>" +
+            "<img src='"+getIcon(getKey(control_type, control.id))+"' class='"+control_type+"--icon'>"
+        ;
+        control_list.appendChild(control_item);
+    });
+};
+
+document.getElementById("controller-select").addEventListener('change', (event) => {
+    updateControlsDisplay(event.target[event.target.selectedIndex].value);
+});
+
+document.addEventListener('keydown', function(event)
+{
+    console.log("Keydown : '"+event.key+"'");
+    console.log(getIcon(event.key));
+});
+
+function getIcon(key)
+{
+    console.log("Getting icon for : "+key);
+    if(key === " ")
+    {
+        key = "Space";
+    }
+
+    if(key.length === 1)
+        key = key.toUpperCase();
+    
+    return "../icons/controls/Keyboard/Light/"+key+".png";
+}
+updateControlsDisplay('keyboard');
+
 const keyListener = (event) => {
-    if(configKeyboard.LEFT === event.keyCode)
+    event.preventDefault();
+    if(configKeyboard.LEFT === event.key)
     {
         game.movePieceLeft();
     }
-    else if(configKeyboard.RIGHT === event.keyCode)
+    else if(configKeyboard.RIGHT === event.key)
     {
         game.movePieceRight();
     }
-    else if(configKeyboard.DOWN === event.keyCode)
+    else if(configKeyboard.DOWN === event.key)
     {
         if (event.preventDefault)
             event.preventDefault();
 
         game.dropPiece();
     }
-    else if(configKeyboard.HOLD === event.keyCode)
+    else if(configKeyboard.HOLD === event.key)
     {
         game.holdPiece();
     }
-    else if(configKeyboard.PAUSE === event.keyCode)
+    else if(configKeyboard.PAUSE === event.key)
     {
         game.pause();
     }
-    else if(configKeyboard.ROTATE_CLOCKWISE === event.keyCode)
+    else if(configKeyboard.ROTATE_CLOCKWISE === event.key)
     {
         game.rotatePieceClockwise();
     }
-    else if(configKeyboard.ROTATE_COUNTER_CLOCKWISE === event.keyCode)
+    else if(configKeyboard.ROTATE_COUNTERCLOCKWISE === event.key)
     {
         game.rotatePieceCounterClockwise();
     }
